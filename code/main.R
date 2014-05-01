d8df4b27258563c9c7223b40d6c6c019144c5f5b
naarkhoo <alireza.kashanipour@gmail.com>
2014-04-30 20:09:06 -0400

    modification in Pablo's office

--- code/main.R
+++ code/main.R
@@ -27,21 +27,19 @@ DISSECTOR_create_components.v1(            #  Project an input dataset into comp
 
 
 source("/home/superiois/Downloads/projectx3/DLBCL_NMF/code/DISSECTOR_lib.v3.R")
-
 DISSECTOR_make_heatmap_of_matrix.v1(
 	input_dataset            = "/home/superiois/Downloads/projectx3/DLBCL_NMF/outputs/compound_A375_7.H_w.gct", # Input dataset (GCT).
 	annot_file               = c("/home/superiois/Downloads/projectx3/DLBCL_NMF/data/compound_classes_A375_7.v3.csv", "Sample", "Type", T),
 	transpose_data           = F,           # Transpose input matrix
 	append_annot             = T,           # Append annotation to column names
 	sorting_method           = "HC",        # Sorting method for cols inside phenotype: MDS (Multi_dimensinal Scaling) or HC (Hiererachical Clustering)  
-	output_plot_landscape    = "/home/superiois/Downloads/projectx3/DLBCL_NMF/plots/clique_DLBCL_n978x323.H_w_LPLOT.pdf",  # Output (PDF) file
-	output_plot_portrait     = "/home/superiois/Downloads/projectx3/DLBCL_NMF/plots/clique_DLBCL_n978x323.H_w_PPLOT.pdf")  # Output (PDF) file
+	output_plot_landscape    = "/home/superiois/Downloads/projectx3/DLBCL_NMF/plots/A375_DLBCL_n978x323.H_w_LPLOT.pdf",  # Output (PDF) file
+	output_plot_portrait     = "/home/superiois/Downloads/projectx3/DLBCL_NMF/plots/A375_DLBCL_n978x323.H_w_PPLOT.pdf")  # Output (PDF) file
 
 
 
 
 source("/home/superiois/Downloads/projectx3/DLBCL_NMF/code/DISSECTOR_lib.v3.R")
-
 DISSECTOR_project_dataset.v1(     # Project a dataset in the space defined by a W matrix
 	input_dataset           = "/home/superiois/Downloads/projectx3/DLBCL_NMF/data/rnaseq.v3.gct",  # Input dataset (GCT) (RNA seq should be the input)
 	input_normalization     = "rank",     # Normalization for the input dataset: "rank"
@@ -53,7 +51,6 @@ DISSECTOR_project_dataset.v1(     # Project a dataset in the space defined by a 
 
 
 source("/home/superiois/Downloads/projectx3/DLBCL_NMF/code/DISSECTOR_lib.v3.R")
-
 DISSECTOR_make_heatmap_of_matrix.v1(
 	input_dataset            = "/home/superiois/Downloads/projectx3/DLBCL_NMF/outputs/DLBCL_OE_L1000_NMF_TA.OE002_PC3_72H_H.gct", # Input dataset (GCT).
 	annot_file               = c("/home/superiois/Downloads/projectx3/DLBCL_NMF/data/CCLE_Kim_Tamayo_96AffyCEL_FAZES_Oct12.v2.txt","CCLE_name", "Site.Primary", F),
@@ -64,18 +61,19 @@ DISSECTOR_make_heatmap_of_matrix.v1(
 	output_plot_portrait     = "/home/superiois/Downloads/projectx3/DLBCL_NMF/plots/ccle_DLBCL_H_proj_PPLOT.v1.pdf")  # Output (PDF) file
 
 
-source("/home/superiois/Downloads/projectx3/DLBCL_NMF/code/DISSECTOR_lib.v3.R")
 
+source("/home/superiois/Downloads/projectx3/DLBCL_NMF/code/DISSECTOR_lib.v3.R")
 DISSECTOR_extract_gene_sets.v1(            #  Extract/Define gene sets from a W matrix
 	input_dataset      	 = "/home/superiois/Downloads/projectx3/DLBCL_NMF/outputs/compound_A375_7.W.gct",          # Input W matrix (GCT file)
-	min.genes.per.comp 	 = 50,          # Minimun number of genes per component gene set
-	max.genes.per.comp 	 = 150,         # Maximun number of genes per component gene set
+	min.genes.per.comp 	 = 5,          # Minimun number of genes per component gene set
+	max.genes.per.comp 	 = 15,         # Maximun number of genes per component gene set
 	output_gene_sets_file 	 = "/home/superiois/Downloads/projectx3/DLBCL_NMF/outputs/DLBCL_classes_W_gene_sets.v1.gmt", # Output GMT file with gene sets
 	output_plots          	 = "/home/superiois/Downloads/projectx3/DLBCL_NMF/plots/DLBCL_classes_A375_K7.v1.pdf")     # Output PDF file with W and H plots
 
 
 
-#### under process
+
+### under process
 source("/home/superiois/Downloads/projectx3/DLBCL_NMF/code/DISSECTOR_lib.v3.R")
 source("/home/superiois/Downloads/projectx3/DLBCL/Dissector/FS.library.v8.6.R")
 
